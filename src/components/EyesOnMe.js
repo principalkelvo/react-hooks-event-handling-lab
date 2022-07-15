5fd3275e8f37a9cf6f256ca747e6f39171577dfe
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-15 16:59:12 +0300

    adds button

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function focus(event) {
+    console.log("Good!");
+  }
+  function blur(event) {
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+  );
+}
+
+export default EyesOnMe;
